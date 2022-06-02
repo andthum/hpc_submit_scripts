4133f0ad4d02efb1b9a479d869ceb57702966f02
andthum <56444687+andthum@users.noreply.github.com>
2022-06-02 12:43:34 +0200

    [submit_gmx_mdrun.py]: Import `python/gmx.py`
    Remove the function `gmx_get_nsteps_from_mdp` and instead import it from
    `python/gmx.py`.

--- simulation/gmx/submit_gmx_mdrun.py
+++ simulation/gmx/submit_gmx_mdrun.py
@@ -314,58 +314,6 @@ import sys
 import warnings
 
 
-def gmx_get_nsteps_from_mdp(infile):
-    """
-    Extract the maximum number of simulation steps of an Gromacs MD
-    simulation from the `.mpd file
-    <https://manual.gromacs.org/documentation/current/user-guide/mdp-options.html>`__.
-
-    Parameters
-    ----------
-    infile : str
-        Name of the .mdp file.
-
-    Returns
-    -------
-    nsteps : int
-        Maximum number of simulation steps specified in the .mdp file.
-
-    Raises
-    ------
-    ValueError
-        If the input file does not contain a line that starts with
-        'nsteps' or if 'nsteps' is not followed by an equal (=) sign.
-    """  # noqa: W505,E501
-    with open(infile, "r") as file:
-        found_nsteps = False
-        for i, line in enumerate(file):
-            line = line.strip()
-            if line.startswith("nsteps"):
-                found_nsteps = True
-                line_nsteps = line
-                line_num = i + 1
-                # nsteps can be defined multiple times in an .mdp file.
-                # From
-                # https://manual.gromacs.org/documentation/current/reference-manual/file-formats.html#mdp  # noqa: W505,E501
-                # "The ordering of the items is not important, but if
-                # you enter the same thing twice, the last is used."
-                # => Do not break the loop after the first occurence of
-                #    'nsteps'.
-    if not found_nsteps:
-        raise ValueError(
-            "Could not fine a line in file '{}' that starts with"
-            " 'nsteps'".format(infile)
-        )
-    if "=" not in line_nsteps:
-        raise ValueError(
-            "Line {} in file '{}' starts with 'nsteps' but does not contain an"
-            " equal (=) sign".format(line_num, infile)
-        )
-    nsteps = line_nsteps.split("=")[1]
-    nsteps = nsteps.split(";")[0]  # Remove potential comments
-    return int(nsteps)
-
-
 def extract_ints_from_str(s):
     """
     Extract all integers from a string.
@@ -454,6 +402,16 @@ def rm_option(cmd, option):
                 cmd_list.pop(ix)
         cmd = " ".join(cmd_list)
     return cmd
+file_root = os.path.abspath(os.path.dirname(__file__))
+python_dir = os.path.join(file_root, "../../python")
+if not os.path.isdir(python_dir):
+    raise FileNotFoundError(
+        "No such directory: '{}'.  This might happen if you change the"
+        " directory structure of this project".format(python_dir)
+    )
+sys.path.insert(1, python_dir)
+# Third-party libraries
+import gmx  # noqa: E402
 
 
 if __name__ == "__main__":  # noqa: C901
@@ -872,8 +830,8 @@ if __name__ == "__main__":  # noqa: C901
             " source file relative to the lmod directory of this project with"
             " --gmx-lmod".format(gmx_lmod)
         )
-    nsteps = gmx_get_nsteps_from_mdp(
-        args.SETTINGS + "_" + args.SYSTEM + ".mdp"
+    nsteps = gmx.get_nsteps_from_mdp(
+        args["settings"] + "_" + args["system"] + ".mdp"
     )
     pos_args_list = [
         bash_dir,
