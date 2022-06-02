a4e798b2f681e64668b1d0f650c40ded5254037b
andthum <56444687+andthum@users.noreply.github.com>
2022-06-02 12:47:51 +0200

    [python/strng.py]: Add Function `extract_ints_from_str`
    Copy the function `extract_ints_from_str` from
    `simulation/gmx/submit_gmx_mdrun.py` to `python/strng.py`.

--- python/strng.py
+++ python/strng.py
@@ -3,3 +3,32 @@
 
 
 """Python functions that operate on or manipulate strings."""
+
+
+def extract_ints_from_str(s):
+    """
+    Extract all integers from a string.
+
+    Parameters
+    ----------
+    s : str
+        The input string.
+
+    Returns
+    -------
+    ints : list
+        List of integers in `s`.
+
+    Examples
+    --------
+    >>> extract_ints_from_str('I have 2 apples and 4 pears')
+    [2, 4]
+    >>> extract_ints_from_str('I have 2.5 apples and 4 pears')
+    [4]
+    >>> extract_ints_from_str('I have no apples and no pears')
+    []
+    >>> extract_ints_from_str('I have -1 apples and -2.5 pears')
+    []
+    """
+    ints = [int(i) for i in s.split() if i.isdigit()]
+    return ints
