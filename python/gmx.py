04727ff964bf74ff180e0302a61368dcd6574cc2
andthum <56444687+andthum@users.noreply.github.com>
2022-06-02 12:37:12 +0200

    [python/gmx.py]: Create
    Create a separate Python module for functions and classes that are
    related molecular dynamics simulations that were performed with Gromacs.

---
+++ python/gmx.py
@@ -0,0 +1,10 @@
+# MIT License
+# Copyright (c) 2021, 2022  All authors listed in the file AUTHORS.rst
+
+
+"""
+Python functions related to molecular dynamics (MD) simulations
+performed with Gromacs_.
+
+.. _Gromacs: https://www.gromacs.org/
+"""
