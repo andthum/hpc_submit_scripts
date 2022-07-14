1b78ca34f3764053737b6f9a95212b41342a77ac
andthum <56444687+andthum@users.noreply.github.com>
2022-07-14 17:12:57 +0200

    [lmod/palma/20*/gnuplot*.sh]: Create
    Add further bash files that can be sourced to load gnuplot on Palma2.

---
+++ lmod/palma/2019b/gnuplot5-2-8.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+module load palma/2019b || exit
+module load GCCcore/8.3.0 || exit
+module load gnuplot/5.2.8 || exit
---
+++ lmod/palma/2021a/gnuplot5-4-2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+module load palma/2021a || exit
+module load GCCcore/10.3.0 || exit
+module load gnuplot/5.4.2 || exit
