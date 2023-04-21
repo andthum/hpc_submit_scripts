3f1d70aae9088f80ad609954a63e140207ebec21
andthum <56444687+andthum@users.noreply.github.com>
2023-04-21 17:50:25 +0200

    [subvolume_charge.sh]: Increase Memory from 4G to 8G

--- analysis/lintf2_ether/mdt/subvolume_charge.sh
+++ analysis/lintf2_ether/mdt/subvolume_charge.sh
@@ -5,7 +5,7 @@
 #SBATCH --output="mdt_subvolume_charge_slurm-%j.out"
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=4G
+#SBATCH --mem=8G
 # The above options are only default values that can be overwritten by
 # command-line arguments
 
