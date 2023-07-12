c9d06b83059e920933d3aecf5ccfb179855df16b
andthum <56444687+andthum@users.noreply.github.com>
2023-07-12 10:31:26 +0200

    [.cleanup_mdt_analyses.sh]: Fix Gathering
    Make function variables local to the function scope.

--- analysis/lintf2_ether/mdt/.cleanup_mdt_analyses.sh
+++ analysis/lintf2_ether/mdt/.cleanup_mdt_analyses.sh
@@ -7,46 +7,47 @@
 
 gather() {
     # Gather output of similar analyses scripts.
-    root="${1}"
+    local root="${1}"
     if [[ -d ${root} ]]; then
         echo "WARNING: Directory already exists: '${root}'"
         return 0
     fi
-    if [[ -n $(find . -maxdepth 1 -type d -name "${root}_*slurm-[0-9]*" -print -quit) ]]; then
+    if [[ -n $(find . -maxdepth 1 -type d -name "${root}*_slurm-[0-9]*" -print -quit) ]]; then
         mkdir "${root}/" || exit
-        mv "${root}"_*slurm-[0-9]*/ "${root}/" || exit
+        mv "${root}"*_slurm-[0-9]*/ "${root}/" || exit
         cd "${root}/" || exit
-        mv "${root}"_*slurm-[0-9]*/* ./ || exit
-        rm -r "${root:?}"_*slurm-[0-9]*/ || exit
+        mv "${root}"*_slurm-[0-9]*/* ./ || exit
+        rm -r "${root:?}"*_slurm-[0-9]*/ || exit
         cd ../ || exit
     else
-        echo "WARNING: No directory matching pattern '${root}_*slurm-[0-9]*'"
+        echo "WARNING: No directory matching pattern '${root}*_slurm-[0-9]*'"
     fi
 }
 
 gather_cmps() {
     # Gather output of similar analyses scripts ordered by compounds
     # into sub-directories.
-    root="${1}"
-    cmps="${2}"
+    local root="${1}"
+    local cmps="${2}"
 
     # Create "root" directory.
     if [[ -d ${root} ]]; then
         echo "WARNING: Directory already exists: '${root}'"
         return 0
+    else
+        mkdir "${root}/" || exit
     fi
 
     # Move all "compound" directories to the root directory.
-    mkdir "${root}/" || exit
     for cmp in ${cmps}; do
-        if [[ -n $(find . -maxdepth 1 -type d -name "${root}_${cmp}_*slurm-[0-9]*" -print -quit) ]]; then
-            mv "${root}_${cmp}"_*slurm-[0-9]*/ "${root}/" || exit
+        if [[ -n $(find . -maxdepth 1 -type d -name "${root}_${cmp}*_slurm-[0-9]*" -print -quit) ]]; then
+            mv "${root}_${cmp}"*_slurm-[0-9]*/ "${root}/" || exit
         fi
     done
 
     # Check if the root directory is empty.
     if [[ -n $(find "${root}/" -prune -empty) ]]; then
-        echo "WARNING: No directory matching pattern '${root}_<compound>_*slurm-[0-9]*'"
+        echo "WARNING: No directory matching pattern '${root}_<compound>*_slurm-[0-9]*'"
         rm -r "${root:?}/" || exit
         return 0
     fi
@@ -79,6 +80,6 @@ gather "contact_hist"
 gather_cmps "discrete_hex" "Li NBT OBT OE"
 gather "discrete-z"
 
-gather_cmps "msd_layer" "Li NBT NTf2 OBT OE NTf2 ether"
+gather_cmps "msd_layer" "Li NBT OBT OE NTf2 ether"
 gather "msd_at_coord_change"
 gather "msd"
