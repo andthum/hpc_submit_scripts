f78268ebdcad134aca5a2f3a12adc8cd4d9bf657
andthum <56444687+andthum@users.noreply.github.com>
2023-12-01 15:39:45 +0100

    [.cleanup_gmx_analyses.sh]: Create Compound Subdirectories for `densmap-z` and `rdf_slab-z`

--- analysis/lintf2_ether/gmx/.cleanup_gmx_analyses.sh
+++ analysis/lintf2_ether/gmx/.cleanup_gmx_analyses.sh
@@ -24,13 +24,51 @@ gather() {
     fi
 }
 
+gather_cmps() {
+    # Gather output of similar analyses scripts ordered by compounds
+    # into sub-directories.
+    local root="${1}"
+    local cmps="${2}"
+
+    # Create "root" directory.
+    if [[ -d ${root} ]]; then
+        echo "WARNING: Directory already exists: '${root}'"
+        return 0
+    else
+        mkdir "${root}/" || exit
+    fi
+
+    # Move all "compound" directories to the root directory.
+    for cmp in ${cmps}; do
+        if [[ -n $(find . -maxdepth 1 -type d -name "${root}_${cmp}*_slurm-[0-9]*" -print -quit) ]]; then
+            mv "${root}_${cmp}"*_slurm-[0-9]*/ "${root}/" || exit
+        fi
+    done
+
+    # Check if the root directory is empty.
+    if [[ -n $(find "${root}/" -prune -empty) ]]; then
+        echo "WARNING: No directory matching pattern '${root}_<compound>*_slurm-[0-9]*'"
+        rm -r "${root:?}/" || exit
+        return 0
+    fi
+
+    # Gather all compound analyses.
+    cd "${root}/" || exit
+    for cmp in ${cmps}; do
+        gather "${root}_${cmp}"
+    done
+    cd ../ || exit
+}
+
 gather "density-z"
-gather "densmap-z"
 gather "energy"
 gather "make_ndx"
 gather "msd"
 gather "polystat"
 gather "potential-z"
-gather "rdf_slab-z"
-gather "rdf"
 gather "trjconv"
+
+gather_cmps "densmap-z" "gra Li NBT OBT OE"
+
+gather_cmps "rdf_slab-z" "Li-com NTf2-com ether-com Li NBT OE"
+gather "rdf"
